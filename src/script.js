689a9107976e3f73b1a8278da0c9a353cc7959d4
kelvin-kariuki <Kariuki405@gmail.com>
2022-06-30 00:57:14 +0300

    Heroku deploy

--- src/script.js
+++ src/script.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded",function(){
     // console.log("The DOM has loaded")
    
 })
-const baseUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=9e96a68fba5c46d0a65ef2d1823fd1d1"
-const proxyUrl = "https://cors-anywhere.herokuapp.com/"
-const url =  `${proxyUrl}${baseUrl}`;
+// const baseUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=9e96a68fba5c46d0a65ef2d1823fd1d1"
+// const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+// const url =  `${proxyUrl}${baseUrl}`;
 function article (){
-fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=9e96a68fba5c46d0a65ef2d1823fd1d1")
+fetch("https://shrouded-beach-63756.herokuapp.com/articles")
   .then(response => response.json())
   .then(data => {
     let html = " ";
